75360a14c1b178fc817863b8551fcd2d5f060a40
Ed Parcell <ed.parcell@cobaltquantware.com>
2010-09-15 17:49:53 +0100

    Added requirements for hnscraper to setup.py

--- src/setup.py
+++ src/setup.py
@@ -8,4 +8,7 @@ setup(
     description='Library to scrape content from the Hacker News website',
     author='Ed Parcell',
     author_email='ed.parcell@cobaltquantware.com',
-    packages=['hnscraper'])
+    packages=['hnscraper'],
+    install_requires=["mechanize >= 0.1.7",
+                      "BeautifulSoup >= 3.0.7"
+                      ])
