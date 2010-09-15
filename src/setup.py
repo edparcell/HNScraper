0d813164cef30d45ee8bfb79cdd60ae74bd9e43d
Ed Parcell <ed.parcell@cobaltquantware.com>
2010-09-15 17:40:16 +0100

    Added setup.py to allow installation of hnscraper with setuptools.

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='hnscraper',
+    version='0.1',
+    description='Library to scrape content from the Hacker News website',
+    author='Ed Parcell',
+    author_email='ed.parcell@cobaltquantware.com',
+    packages=['hnscraper'])
